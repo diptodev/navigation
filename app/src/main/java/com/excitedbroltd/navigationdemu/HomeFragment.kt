0338a4e731eea8fbe1386ce374537711e7a6ec12
diptodev <dipankardebnathcse@yahoo.com>
2023-05-31 21:35:10 +0600

    navigation WORKING

--- app/src/main/java/com/excitedbroltd/navigationdemu/HomeFragment.kt
+++ app/src/main/java/com/excitedbroltd/navigationdemu/HomeFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.os.bundleOf
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.excitedbroltd.navigationdemu.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -14,8 +17,24 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_home, container, false
+        )
+
         return binding.root
     }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.btn).setOnClickListener {
+            val bundle = bundleOf("userName" to binding.name.text.toString())
+            it.findNavController().navigate(R.id.action_homeFragment_to_yardFragment, bundle)
+        }
+
+
+    }
+
 
 }
--- app/src/main/java/com/excitedbroltd/navigationdemu/YardFragment.kt
+++ app/src/main/java/com/excitedbroltd/navigationdemu/YardFragment.kt
@@ -1,9 +1,11 @@
 package com.excitedbroltd.navigationdemu
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.excitedbroltd.navigationdemu.databinding.FragmentYardBinding
@@ -18,5 +20,11 @@ class YardFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val value = requireArguments().getString("userName")
+        view.findViewById<TextView>(R.id.textView).text = value.toString()
+        Log.d("LIVED", "onViewCreated: $value")
 
+    }
 }
