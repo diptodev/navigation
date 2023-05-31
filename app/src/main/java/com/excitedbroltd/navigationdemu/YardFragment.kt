3678ff14def70606ec96c511d94e20c7d54d5382
diptodev <dipankardebnathcse@yahoo.com>
2023-05-31 21:21:38 +0600

    navigation fragment added

---
+++ app/src/main/java/com/excitedbroltd/navigationdemu/YardFragment.kt
@@ -0,0 +1,22 @@
+package com.excitedbroltd.navigationdemu
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.excitedbroltd.navigationdemu.databinding.FragmentYardBinding
+
+class YardFragment : Fragment() {
+    private lateinit var binding: FragmentYardBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_yard, container, false)
+        return binding.root
+    }
+
+
+}
