a4a5b827c84e33716088123af71f6cd1c90ecc19
diptodev <dipankardebnathcse@yahoo.com>
2023-05-31 22:13:40 +0600

    clean code and fix some bug

--- app/src/main/java/com/excitedbroltd/navigationdemu/HomeFragment.kt
+++ app/src/main/java/com/excitedbroltd/navigationdemu/HomeFragment.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -14,26 +14,29 @@ import com.excitedbroltd.navigationdemu.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_home, container, false
         )
+        binding.btn.setOnClickListener {
+            val text = binding.name.text.toString()
+            val bundle = bundleOf("userName" to text)
+            if (text.isNotEmpty()) {
+                it.findNavController().navigate(R.id.action_homeFragment_to_yardFragment, bundle)
+            } else {
+                Toast.makeText(container?.context, "No name entered", Toast.LENGTH_SHORT).show()
+            }
+        }
 
         return binding.root
     }
 
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<Button>(R.id.btn).setOnClickListener {
-            val bundle = bundleOf("userName" to binding.name.text.toString())
-            it.findNavController().navigate(R.id.action_homeFragment_to_yardFragment, bundle)
-        }
-
-
+    override fun onPause() {
+        super.onPause()
+        binding.name.text.clear()
     }
 
 
--- app/src/main/java/com/excitedbroltd/navigationdemu/YardFragment.kt
+++ app/src/main/java/com/excitedbroltd/navigationdemu/YardFragment.kt
@@ -1,11 +1,9 @@
 package com.excitedbroltd.navigationdemu
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.excitedbroltd.navigationdemu.databinding.FragmentYardBinding
@@ -17,14 +15,9 @@ class YardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_yard, container, false)
+        val value = requireArguments().getString("userName")
+        binding.textView.text = value.toString()
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val value = requireArguments().getString("userName")
-        view.findViewById<TextView>(R.id.textView).text = value.toString()
-        Log.d("LIVED", "onViewCreated: $value")
-
-    }
 }
